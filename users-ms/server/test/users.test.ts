dd381d2b034c8372d6af73813efb509385036c31
Gregor Lennie <gregorlennie@hotmail.co.uk>
2023-02-21 20:22:33 +0000

    Add more user endpoint tests

--- users-ms/server/test/users.test.ts
+++ users-ms/server/test/users.test.ts
@@ -1,11 +1,107 @@
 import request from "supertest";
-import dbPool from "../database/dbPool";
 import app from "../App";
+import dbPool from "../database/dbPool";
+import User from "../models/User";
 
-describe("Example test", () => {
-    it("should return a 200 status code", async () => {
-        const res = await request(app).get("/users");
-        expect(res.status).toBe(200);
+const application = request(app);
+const token = "";
+
+describe("Users", () => {
+    describe("GET /users", () => {
+        it.skip("should return a 401 status code if a token is missing from request headers", async () => {
+            const res = await application.get("/users");
+            expect(res.status).toBe(401);
+        });
+
+        it("should return a 200 if request is successful", async () => {
+            const res = await application
+                .get("/users")
+                .set("Authorization", `Bearer ${token}`);
+            expect(res.status).toBe(200);
+        });
+
+        it.skip("should return an array of users if request is successful", async () => {
+            const res = await application
+                .get("/users")
+                .set("Authorization", `Bearer ${token}`);
+
+            expect(res.body).toHaveLength(1);
+        });
+    });
+
+    describe("POST /users", () => {
+        it.skip("should return a 201 status code when a user is created", async () => {
+            const userRequestBody: User = {
+                firstName: "Tester",
+                lastName: "McTesterson",
+                email: "test@test.com",
+            };
+
+            const res = await application
+                .post("/users")
+                .send(userRequestBody)
+                .set("Authorization", `Bearer ${token}`)
+                .set("Accept", "application/json");
+
+            expect(res.status).toBe(201);
+        });
+
+        it.skip("should return a 401 status code if a token is missing from request headers", async () => {
+            const userRequestBody: User = {
+                firstName: "Tester",
+                lastName: "McTesterson",
+                email: "test@test.com",
+            };
+
+            const res = await application
+                .post("/users")
+                .send(userRequestBody)
+                .set("Accept", "application/json");
+
+            expect(res.status).toBe(401);
+        });
+
+        it.skip("should return a 400 status code if the user first name is missing from request body", async () => {
+            const userRequestBody: Partial<User> = {
+                lastName: "McTesterson",
+                email: "test@test.com",
+            };
+
+            const res = await application
+                .post("/users")
+                .send(userRequestBody)
+                .set("Accept", "application/json");
+
+            expect(res.status).toBe(400);
+        });
+
+        it.skip("should return a 400 status code if the user last name is missing from request body", async () => {
+            const userRequestBody: Partial<User> = {
+                firstName: "Tester",
+                email: "test@test.com",
+            };
+
+            const res = await application
+                .post("/users")
+                .send(userRequestBody)
+                .set("Accept", "application/json");
+
+            expect(res.status).toBe(400);
+        });
+
+        it.skip("should return a 400 status code if the user email is missing from request body", async () => {
+            const userRequestBody: Partial<User> = {
+                firstName: "Tester",
+                lastName: "McTesterson",
+            };
+
+            const res = await application
+                .post("/users")
+                .send(userRequestBody)
+                .set("Accept", "application/json");
+
+            expect(res.status).toBe(400);
+        });
     });
 
     afterAll(async () => {
