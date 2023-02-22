fa304baed1d357bd76c77d9cb7924a7193f56455
Gregor Lennie <gregorlennie@hotmail.co.uk>
2023-02-22 19:59:20 +0000

    Issue jwt to user on successful login

--- users-ms/server/api/auth/authControllers.ts
+++ users-ms/server/api/auth/authControllers.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
+import Jwt from "jsonwebtoken";
+
 import dbPool from "../../database/dbPool";
 import checkPassword from "../../utils/checkPassword";
+
+const TOKEN_EXPIRY = 60 * 15;
 
 const login = async (req: Request, res: Response): Promise<void> => {
     try {
@@ -22,7 +26,12 @@ const login = async (req: Request, res: Response): Promise<void> => {
             });
             return;
         }
-        res.status(200).json({ message: "You have logged in!" });
+
+        const tokenBody = { userUuid: user.uuid };
+        const token = Jwt.sign(tokenBody, "secret", { expiresIn: TOKEN_EXPIRY });
+
+        await dbPool.query("INSERT INTO tokens(user_id, token) VALUES ($1, $2)", [user.id, token]);
+        res.status(200).set("Authorization", token).json({ message: "You have logged in!" });
     } catch (error) {
         console.log(error);
         res.status(500).json({ error, message: "An error occured" });
--- users-ms/server/utils/checkPassword.ts
+++ users-ms/server/utils/checkPassword.ts
@@ -2,7 +2,6 @@ import bcrypt from "bcryptjs";
 
 const checkPassword = async (password: string, hashedPassword: string): Promise<boolean> => {
     const result = await bcrypt.compare(password, hashedPassword);
-    console.log({ result });
     return result;
 };
 
